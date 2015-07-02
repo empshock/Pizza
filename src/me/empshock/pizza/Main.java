697aadba0c086b25de40b6ed2bf28493d8766e81
empshock <mrempshock@hotmail.com>
2015-07-02 00:05:03 -0700

    added onenable

--- src/me/empshock/pizza/Main.java
+++ src/me/empshock/pizza/Main.java
@@ -1,8 +1,13 @@
 package me.empshock.pizza;
 
-
 import me.empshock.pizza.utils.Pizza;
 import org.bukkit.plugin.java.JavaPlugin;
+import java.util.logging.Logger;
 
-public class Main extends JavaPlugin implements Pizza {
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("sup, fag. this is pizza nig");
+    }
 }
